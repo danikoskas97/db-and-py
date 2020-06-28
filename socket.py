2406187047dbcb0a6f8188a5c503c6153053eda2
dani <koskas.dani@gmail.com>
2020-06-28 18:17:02 +0300

    initial commit

--- socket.py
+++ socket.py
@@ -1,3 +1,14 @@
 import socket
 
+host, port = ('', 5566)
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.bind((host, port))
+print("the server is start")
 
+while True:
+    socket.listen(5)
+    conn, address = socket.accept()
+    print("listening")
+
+conn.close()
+socket.close()
