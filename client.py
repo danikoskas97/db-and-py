e5ab11dab29c8bc3a40ede783d50b1d8afd08b46
dani <koskas.dani@gmail.com>
2020-06-28 18:36:31 +0300

    up to date

--- client.py
+++ client.py
@@ -5,8 +5,14 @@ host, port = ('localhost', 5566)
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     socket.connect((host, port))
-    print("client is connected")
+    print("client is now connected")
+
+    data = "welcome iam the client"
+    data = data.encode("utf8")
+    socket.sendall(data)
+
 except ConnectionRefusedError:
+    # if the server is not connected
     print("connection failed !")
 finally:
     socket.close()
